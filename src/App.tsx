0df52dba166b3d1845375f7c53b4b423372ce6b7
otaciliow <otacilio.dml@hotmail.com>
2024-12-06 15:12:46 -0300

    feat: added minified css

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-import './App.css'
+import './App.min.css'
 
 function App() {
 
@@ -9,13 +9,13 @@ function App() {
       <div className="container">
         <h1 className="title">Descubra sua idade</h1>
         <form action="" className="form-idade" id="form-idade">
-          
-          <label htmlFor="nome" className="form-label">Qual é o seu nome?</label>
-          <input type="text" className="form-input" id="nome" placeholder="Digite seu nome" />
-          
-          <label htmlFor="ano-nascimento" className="form-label">Em que ano você nasceu?</label>
-          <input type="text" className="form-input" id="ano-nascimento" placeholder="Digite o ano de nascimento" />
-          
+
+            <label htmlFor="nome" className="form-label">Qual é o seu nome?</label>
+            <input type="text" className="form-input" id="nome" placeholder="Digite seu nome" />
+
+            <label htmlFor="ano-nascimento" className="form-label">Em que ano você nasceu?</label>
+            <input type="text" className="form-input" id="ano-nascimento" placeholder="Digite o ano de nascimento" />
+
           <button className="form-button">Descobrir idade</button>
         </form>
         <p className="info-result"></p>
